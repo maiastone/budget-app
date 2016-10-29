51376ef46e8d0b1c9f82cb9c191d0c92a78df1c3
Maia Stone <maiadov@gmail.com>
2016-10-29 12:20:57 -0600

    initial commit firebase setup

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyAHXAtiBE1W3yYsiSGyhnK7FBVyydAalP8',
+  authDomain: 'chat-app-d59cd.firebaseapp.com',
+  databaseURL: 'https://chat-app-d59cd.firebaseio.com',
+  storageBucket: '',
+  messagingSenderId: '1067075746848'
+};
+
+firebase.initializeApp(config);
