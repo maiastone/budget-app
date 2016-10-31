3db04b4248e3e23a607b80f3072dd22953f427d2
Nick <nicknchambers@gmail.com>
2016-10-31 14:40:36 -0600

    adds firebase and budget states to budgetForm

--- components/application.jsx
+++ components/application.jsx
@@ -8,39 +8,20 @@ const auth = firebase.auth();
 class Application extends React.Component {
   constructor() {
     super();
-    this.setBudget = this.setBudget.bind(this);
-    this.setUserBudget = this.setUserBudget.bind(this);
     this.state = {
-      user: false,
-      userBudget: {
-        title: '',
-        amount: ''
-      }
-    }
+      user: false
+    };
   }
 
   signIn() {
-  let google = new firebase.auth.GoogleAuthProvider()
-    auth.signInWithPopup(google)
+  let google = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(google);
   }
 
   componentDidMount() {
-    const usersRef = firebase.database().ref('users');
     firebase.auth().onAuthStateChanged((user) => {
       this.setState({user});
     });
-  }
-
-  setBudget(e) {
-    let userBudget = this.state.userBudget;
-    const budgetRef = firebase.database().ref(`users/${this.state.user.uid}`);
-    budgetRef.push({userBudget})
-  }
-
-  setUserBudget(e, key) {
-    let userBudget = this.state.userBudget;
-    userBudget[key] = e.target.value;
-    this.setState({userBudget: userBudget})
   }
 
   render() {
@@ -49,7 +30,7 @@ class Application extends React.Component {
          <button className="sign"
           onClick={() => this.signIn()}>Sign In</button>
           <h1>{this.state.user.email}</h1>
-          <BudgetForm setBudget={this.setBudget} setUserBudget={this.setUserBudget}  />
+          <BudgetForm uid={this.state.user.uid}   />
       </div>
     )
   }
--- components/budgetForm.jsx
+++ components/budgetForm.jsx
@@ -1,24 +1,47 @@
 import React from 'react';
+const firebase = require('../firebase');
+
 
 class BudgetForm extends React.Component {
   constructor() {
     super();
+    this.state = {
+      userBudget: {
+        title: '',
+        amount: ''
+      }
+    };
   }
+
+  pushBudget(e) {
+    e.preventDefault();
+    let userBudget = this.state.userBudget;
+    const budgetRef = firebase.database().ref(`users/${this.props.uid}`);
+    budgetRef.push({userBudget});
+  }
+
+  setUserBudget(e, key) {
+    let userBudget = this.state.userBudget;
+    userBudget[key] = e.target.value;
+    this.setState({ userBudget: userBudget });
+  }
+
   render() {
     return(
       <form>
         <input placeholder="budget item"
-          onChange={(e) => this.props.setUserBudget(e, 'title')}>
+          onChange={(e) => this.setUserBudget(e, 'title')}>
         </input>
         <input placeholder="budget amount"
-          onChange={(e) => this.props.setUserBudget(e, 'amount')}>
+          onChange={(e) => this.setUserBudget(e, 'amount')}>
         </input>
         <button className="submitBudget"
-          onClick={(e) => this.props.setBudget(e)}>
+          onClick={(e) => this.pushBudget(e)}>Submit Budget
         </button>
       </form>
     )
   }
 }
+
 
 module.exports = BudgetForm;
