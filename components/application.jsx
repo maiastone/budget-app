0c897d991c629375444cded46f974c0df290fd39
Nick <nicknchambers@gmail.com>
2016-10-31 20:25:37 -0600

    adds basic router functions

--- components/application.jsx
+++ components/application.jsx
@@ -4,17 +4,26 @@ import { pick, map, extend } from 'lodash';
 import BudgetForm from './budgetForm';
 const auth = firebase.auth();
 
+// 1) track the state for the current route
+// 2) based on current route, render the right page
+// 3) provide a callback to transition the route
+
+
+function DashboardPage() {
+  return <div>Im a dashboard</div>
+}
 
 class Application extends React.Component {
   constructor() {
     super();
     this.state = {
-      user: false
+      user: null,
+      route: "dashboard"
     };
   }
 
   signIn() {
-  let google = new firebase.auth.GoogleAuthProvider();
+    let google = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(google);
   }
 
@@ -24,17 +33,31 @@ class Application extends React.Component {
     });
   }
 
+  transitionRoute(route) {
+    this.setState({route});
+  }
+
   render() {
+    let child
+    if (this.state.route === "dashboard") {
+      child = <DashboardPage />;
+    } else if (this.state.route === "budgetForm") {
+      child = <BudgetForm uid={this.state.user.uid}   />
+    }
     return (
       <div>
+          <nav>
+          super sweet nav
+            <button onClick={() => this.transitionRoute("dashboard")}>dashboardpage</button>
+            <button onClick={() => this.transitionRoute("budgetForm")}>budgetForm</button>
+          </nav>
          <button className="sign"
-          onClick={() => this.signIn()}>Sign In</button>
-          <h1>{this.state.user.email}</h1>
-          <BudgetForm uid={this.state.user.uid}   />
+          onClick={this.signIn}>Sign In</button>
+          <h1>{this.state.user && this.state.user.email}</h1>
+          {child}
       </div>
     )
   }
 }
-
 
 module.exports = Application;
