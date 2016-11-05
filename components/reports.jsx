4251e7b1aa588dd62a494b2f9d1d89edb6525115
Maia Stone <maiadov@gmail.com>
2016-11-05 15:18:39 -0600

    Renders each budget in list form

--- components/reports.jsx
+++ components/reports.jsx
@@ -6,13 +6,18 @@ class Reports extends React.Component {
 
   render() {
     const budgets = this.props.budgets;
+    const allBudgetsArray = [];
 
+    for (let i = 0; i < budgets.length; i++) {
+      const allBudgets = this.props.budgets[i].userBudget.budget;
+      allBudgetsArray.push(allBudgets);
+    }
+
+    const allBudgetsList = allBudgetsArray.map(() => <li>{allBudgetsArray}</li>)
 
     return (
       <div>
-        <ul>
-
-        </ul>
+        <ul>{allBudgetsList}</ul>
       </div>
       );
   }
