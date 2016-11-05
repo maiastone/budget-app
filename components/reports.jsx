159ec2594104aafa10a359832f5cfcb6c49408a4
Maia Stone <maiadov@gmail.com>
2016-11-05 15:52:12 -0600

    Renders total monthly budgets and titles

--- components/reports.jsx
+++ components/reports.jsx
@@ -6,17 +6,31 @@ class Reports extends React.Component {
 
   render() {
     const budgets = this.props.budgets;
-    const allBudgetsArray = [];
 
+    const allBudgetsArray = [];
     for (let i = 0; i < budgets.length; i++) {
       const allBudgets = this.props.budgets[i].userBudget.budget;
       allBudgetsArray.push(allBudgets);
     }
 
-    const allBudgetsList = allBudgetsArray.map(() => <li>{allBudgetsArray}</li>)
+    const allBudgetsList = allBudgetsArray.map((budgets, index) => { // eslint-disable-line
+      return <li key={index}>{budgets}</li>;
+    });
+
+    const allTitlesArray = [];
+    for (let i = 0; i < budgets.length; i++) {
+      const allTitles = this.props.budgets[i].userBudget.title;
+      allTitlesArray.push(allTitles);
+    }
+
+    const allTitlesList = allTitlesArray.map((title, index) => { // eslint-disable-line
+      return <li key={index}>{title}</li>;
+    });
+
 
     return (
-      <div>
+      <div className="monthly-list">
+        <ul>{allTitlesList}</ul>
         <ul>{allBudgetsList}</ul>
       </div>
       );
