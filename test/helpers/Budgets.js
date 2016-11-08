e0d178f09f2c20c6918e528fd4dfd84bbbec8f10
Nick <nicknchambers@gmail.com>
2016-11-07 17:23:05 -0700

    Adds mock budget data for testing

---
+++ test/helpers/Budgets.js
@@ -0,0 +1,12 @@
+module.exports = [{ user: { displayName: 'Andrew Crist',
+uid: 'EKttC7OtfhQah28gOR4Lypq2xLH2' },
+budgets: [{ key: 'KVpdN-4cDlFJqeNt5Qp', userBudget: { actualEntry:
+[{ currentDate: '11/16 /2016', expense: 200 },
+{ currentDate: '11/17/2016', expense: 100 }], budget: 1000,
+title: 'kanye' } }, { key: 'KVv7o7XdJuME98wKcM8', userBudget: { actualEntry:
+[{ currentDate: '11/19 /2016', expense: 200 },
+{ currentDate: '11/18/2016', expense: 100 }],
+budget: 1000, title: 'harambe' } }],
+userbudget: { title: 'rabbits', budget: 400,
+actualEntry: [{ currentDate: '11/20 /2016', expense: 200 },
+{ currentDate: '11/21/2016', expense: 100 }] } }];
